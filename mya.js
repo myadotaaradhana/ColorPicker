8b3b83a47d8c13f8a2bfc1c3592ace1fbbf79060
myadotaaradhana <84834322+myadotaaradhana@users.noreply.github.com>
2021-08-18 09:44:31 +0530

    Add files via upload

---
+++ mya.js
@@ -0,0 +1,15 @@
+function makeRed(){
+    document.body.style.backgroundColor="red";
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor="blue";  
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor="green";  
+}
+
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";  
+}
